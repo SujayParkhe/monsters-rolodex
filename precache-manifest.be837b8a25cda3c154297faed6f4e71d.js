03772f0f8008deca225c168ec285925e6f5b7612
lycanzzz <62604089+lycanzzz@users.noreply.github.com>
2020-06-01 04:37:20 +0530

    Updates

---
+++ precache-manifest.be837b8a25cda3c154297faed6f4e71d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46eacbbbe596f06f301b7725a6f39ca7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c665f17982678f5e2e89",
+    "url": "/monsters-rolodex/static/css/main.df1fefd4.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c665f17982678f5e2e89",
+    "url": "/monsters-rolodex/static/js/main.45e70c86.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
